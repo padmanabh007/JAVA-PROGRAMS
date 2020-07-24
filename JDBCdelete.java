21f53a8ff21e7cd087361aa96862f07405a243e2
rajavu007 <61701158+rajavu007@users.noreply.github.com>
2020-07-24 19:38:37 +0530

    Add files via upload

---
+++ JDBCdelete.java
@@ -0,0 +1,38 @@
+package javaJDBC;
+import java.sql.*;
+
+public class JDBCdelete {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		Connection con=null;
+		Statement st=null;
+		ResultSet rst=null;
+		
+		String url="jdbc:mysql://localhost:3306/demo";
+		String user="student";
+		String password="student";
+		
+		try {
+			String sql="delete from student where userid=6";
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection(url, user, password);
+			st=con.createStatement();
+			
+			int count=st.executeUpdate(sql);
+			System.out.println("Deleted the row");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(st!=null) {
+				st.close();
+			}
+			if(con!=null) {
+				con.close();
+			}
+		}
+	}
+
+}
